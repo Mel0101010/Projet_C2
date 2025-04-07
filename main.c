1a86488989167d179681ef526cb93176b93b9ed2
Mel0101010 <Mel0.Super@proton.me>
2025-04-07 15:24:58 +0200

    correction

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+// Created by melo on 4/7/25.
+//
+
+
+
+void declare_connection(){
+    // Initialize the connection
+    char * command = "DECLARE";
+    char * username = "user";
+    char * hostname = "localhost";
+    char * os = "linux";
+}
+
+
+
+int main(){
+
+}
