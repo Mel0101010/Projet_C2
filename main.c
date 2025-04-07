b169928f1e15bc39528ebe3ec8ee0066e6042394
Mel0101010 <Mel0.Super@proton.me>
2025-04-07 16:59:27 +0200

    mise en place de la connexion

--- main.c
+++ main.c
@@ -2,10 +2,49 @@
 // Created by melo on 4/7/25.
 //
 #include "declare.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define SERVER_ADDR "127.0.0.1"
 
 
-int main(){
-    char * declare_string = declare_connection();
-    return EXIT_SUCCESS;
+
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    // Créer un socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Erreur de création de socket \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, SERVER_ADDR, &serv_addr.sin_addr) <= 0) {
+        printf("\nAdresse invalide / Adresse non supportée \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\n Erreur de connexion \n");
+        return -1;
+    }
+
+    char *declare = declare_connection();
+    send(sock, declare, strlen(declare), 0);
+    printf("Message envoyé : %s\n", declare);
+
+    read(sock, buffer, 1024);
+    printf("Message reçu : %s\n", buffer);
+
+    close(sock);
+    return 0;
 }
