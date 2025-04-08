ca75280a920a672441fd7288c5d90f623e5295c1
Mel0101010 <Mel0.Super@proton.me>
2025-04-08 20:18:41 +0200

    encode et decode fonctionnels

---
+++ base64.h
@@ -0,0 +1,9 @@
+//
+// Created by melo on 4/8/25.
+//
+
+#ifndef BASE64_H
+#define BASE64_H
+void encoder_base64(const char *input, char **output);
+void decoder_base64(const char *input, char **output);
+#endif //BASE64_H
